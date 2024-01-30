457f666e055d0827232ccf0ccf7aeb7d0207c13f
Isleyls <isleyls@appstate.edu>
2024-01-30 16:50:02 -0500

    started some practice files to learn the basics of Flutter

---
+++ pup_iq/lib/practice.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+//Lani & Tyler's Project
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('pupIQ'),
+        ),
+      ),
+      ));
+}
+
+/*class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: PurpleBackground(), // Use the PurpleBackground widget as the home
+    );
+  }
+}
+
+class PurpleBackground extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.purple, // Set the background color to purple
+    );
+  }
+}*/
+
