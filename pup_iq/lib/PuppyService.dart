b8ab6017e2d09b4cfa4ab16ea43913033110c652
Tyler Francisco <tylerfrancisco@Tylers-Laptop.local>
2024-03-05 16:06:52 -0500

    made a new file for the puppyService

---
+++ pup_iq/lib/PuppyService.dart
@@ -0,0 +1,64 @@
+import "package:flutter/material.dart";
+import "package:pup_iq/puppy.dart";
+
+
+
+class PuppyList extends ChangeNotifier {
+  List<puppy> puppyList = [
+    puppy("Tobias", "Corgi", 0, 10)
+  ];
+
+  void addPuppy(String name, String breed, String age, String weight) {
+    puppyList.add(puppy(name, breed, int.parse(age), int.parse(weight)));
+  }
+
+  puppy getPuppy(int index) {
+    return puppyList[index];
+  }
+
+  // Returns the list of dogs
+  List<puppy> getData() {
+    return puppyList;
+  }
+
+  // Sets the list of dogs
+  void setData(List<puppy> newList) {
+    puppyList = newList;
+  }
+
+  // Edits the name
+  void editName(puppy toEdit, String newName) {
+    int index = puppyList.indexOf(toEdit);
+
+    if (index != -1) {
+      puppyList[index].setName(newName);
+    }
+  }
+
+  // edits the weight
+  void editWeight(puppy toEdit, String newWeight) {
+    int index = puppyList.indexOf(toEdit);
+
+    if (index != -1) {
+      puppyList[index].setWeight(int.parse(newWeight));
+    }
+  }
+
+  // edits the Breed
+  void editBreed(puppy toEdit, String newBreed) {
+    int index = puppyList.indexOf(toEdit);
+
+    if (index != -1) {
+      puppyList[index].setBreed(newBreed);
+    }
+  }
+
+  // edits the Age
+  void editAge(puppy toEdit, String newAge) {
+    int index = puppyList.indexOf(toEdit);
+
+    if (index != -1) {
+      puppyList[index].setWeight(int.parse(newAge));
+    }
+  }
+}
